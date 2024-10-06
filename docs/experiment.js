26216acf3c8619d2f3fdb59cf4022440d2175f25
Lynde Folsom <80857202+lynde-m-folsom@users.noreply.github.com>
2024-10-06 12:43:22 -0700

    Update experiment.js

--- docs/experiment.js
+++ docs/experiment.js
@@ -142,7 +142,7 @@ const set_identifier = path.split('/').slice(-2, -1)[0]; // This should give us 
 // Define the set assignment based on the identifier
 //const set_run = set_identifier || 'SetA1';  // Default to 'SetA1' if not found
     // set run
-const setRun = 'SetC3'
+const set_run = 'SetC3'
 
 //console.log(set_run);
 // Now we use that info in the create_tv_array function for filtering the stimuli set
