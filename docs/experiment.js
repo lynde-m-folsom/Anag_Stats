233817db1c445b9c46046dfeaee0f7ea8b411a52
Lynde Folsom <80857202+lynde-m-folsom@users.noreply.github.com>
2024-10-08 15:29:10 -0700

    modified:   docs/experiment.js

--- docs/experiment.js
+++ docs/experiment.js
@@ -164,7 +164,7 @@ const blockA = {
             allow_blanks: false,
             check_answers: true,
             mistake_fn: handleMistake,
-            trial_duration: 300000,
+            trial_duration: 30000,
             prompt: 'Press enter to continue',
             on_finish: function(data) {
                 jsPsych.setProgressBar((data.trial_index - 1) / (timeline.length + tv_array.length));
